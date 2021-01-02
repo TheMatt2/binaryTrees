895dabd71ed78834249a2d7c832917659aedf2a1
TheMatt2 <TheMatt2@users.noreply.github.com>
2021-01-01 19:25:24 -0500

    Add clearable_stack
    Unused at the current time.

---
+++ clearable_stack.h
@@ -0,0 +1,17 @@
+#include <stack>
+
+/**
+ * An extension to the standard stack that requires the container
+ * to also be clearable.
+ *
+ * This allows for the stack to be cleared in a more efficient manner.
+ * @tparam T type of value to be held in the container
+ * @tparam Container the internal container type to be used
+ */
+template <class T, class Container = std::deque<T>>
+class clearable_stack: public std::stack<T, Container> {
+  public:
+    void clear() {
+        std::stack<T, Container>::c.clear();
+    }
+};
